e0dae3a0eeb865cd89412e400ba8a39cd1b96fba
Paula Krupa <paula.krupa@gmail.com>
2020-09-25 20:26:10 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import React from 'react';
-import './App.css';
+import "./App.css";
 
-// mylinks:
+let Array = [];
 
-// Make it Holy:
-// https://youtu.be/rhJEAEeYmIM?list=PLCZ4SfWipa-oX7g7tWVw4SOQlpoORVRhY
-//
-// One:
-// https://youtu.be/yPCVBWifC6U?list=PLCZ4SfWipa-oX7g7tWVw4SOQlpoORVRhY
-//
-// Dance:
-// https://youtu.be/lEnzIF6iuJw?list=PLCZ4SfWipa-oX7g7tWVw4SOQlpoORVRhY
-
+for (let i=1;i<=100;i++) {
+    if (i%2===0 && i%5===0) {
+        console.log("FizzBuzz")
+        Array.push(<div>FizzBuzz</div>)
+    } else if (i%2===0) {
+        console.log("Fizz")
+        Array.push(<div>Fizz</div>)
+    } else if (i%5===0) {
+        console.log("Buzz")
+        Array.push(<div>Buzz</div>)
+    } else {
+        console.log(i)
+        Array.push(<div>{i}</div>)
+    }
+}
 
 function App() {
-
-    const onelink = <a href="https://youtu.be/yPCVBWifC6U?list=PLCZ4SfWipa-oX7g7tWVw4SOQlpoORVRhY" target="_blank">Song of a mysterious prince</a>;
-    const twolink = <a href="https://youtu.be/lEnzIF6iuJw?list=PLCZ4SfWipa-oX7g7tWVw4SOQlpoORVRhY" target="_blank">Dance of a hateful man</a>;
-    const threelink = <a href="https://youtu.be/rhJEAEeYmIM?list=PLCZ4SfWipa-oX7g7tWVw4SOQlpoORVRhY" target="_blank">Super
-        sad sounds for a sad romance</a>;
-
-    function displayOnLoad() {
-        console.log('loaded')
-    }
-
     return (
-        <div onLoad={displayOnLoad}>
-            Best videos:<br/>
-            {onelink}<br/>
-            {twolink}<br/>
-            {threelink}
-        </div>
+        <div>{Array}</div>
     );
 }
 
